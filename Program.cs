7800e39a726a4ea3b426e7326f3c4c79ed1d5a5a
nilgun <129064182+nilgunnb@users.noreply.github.com>
2023-04-11 12:03:35 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿
+BMW bmw = new BMW(true, "dave05", 999, 9, 1000, 999);
+
+bmw.GetFullInfo();
+
+bmw.GetPropertyInfoBMW(true);
+
+Mercedes mercedes = new Mercedes(true, "hell1050", 999, 9, 1000, 999);
+
+mercedes.GetFullInfo();
+
+mercedes.GetPropertyInfoMercedes(true);
+
 
 
 
