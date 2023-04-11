fe8a60a767812ab034b5c6be8812f5e00c6c0b56
nilgun <129064182+nilgunnb@users.noreply.github.com>
2023-04-11 14:56:13 +0400

    Update BMW.cs

--- BMW.cs
+++ BMW.cs
@@ -1,13 +1,13 @@
-﻿    class BMW:Cars
-    {
+﻿    class BMW : Cars
+{
     public bool IsMpower;
 
-   
-    public BMW (bool IsMpower, string Model, int TopSpeed, double Engine, double Kilometers, int HorsePower):base(Model, TopSpeed, Engine, Kilometers, HorsePower)
+
+    public BMW(bool IsMpower, string Model, int TopSpeed, double Engine, double Kilometers, int HorsePower) : base(Model, TopSpeed, Engine, Kilometers, HorsePower)
     {
         this.IsMpower = IsMpower;
     }
-    public void GetPropertyInfoBMW(bool IsMpower)
+    public void GetFullInfoBMW()
     {
         if (IsMpower)
         {
@@ -17,7 +17,7 @@
         {
             Console.WriteLine("Bunun IsMPoweri yoxdur");
         }
+        Console.WriteLine($"{this.Model},{this.TopSpeed},{this.Engine},{this.Kilometers},{this.HoursePower}");
     }
 
-    }
-
+}
