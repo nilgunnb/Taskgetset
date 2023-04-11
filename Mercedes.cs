eb973b8712a58015c051db5cd9c54f095a5768ec
nilgun <129064182+nilgunnb@users.noreply.github.com>
2023-04-11 14:55:32 +0400

    Update Mercedes.cs

--- Mercedes.cs
+++ Mercedes.cs
@@ -1,13 +1,13 @@
-﻿    class Mercedes:Cars
-    {
+class Mercedes : Cars
+{
     public bool IsAmg;
-    public Mercedes (bool isAmg, string Model, int TopSpeed, double Engine, double Kilometers, int HorsePower) : base (Model, TopSpeed, Engine, Kilometers, HorsePower)
+    public Mercedes(bool isAmg, string Model, int TopSpeed, double Engine, double Kilometers, int HorsePower) : base(Model, TopSpeed, Engine, Kilometers, HorsePower)
     {
-        this.IsAmg= isAmg;
+        this.IsAmg = isAmg;
     }
-    public void GetPropertyInfoMercedes(bool  isAmg)
+    public void GetFullInfoMercedes()
     {
-        if (isAmg)
+        if (IsAmg)
         {
             Console.WriteLine("Bunun IsAmg-si var");
         }
@@ -15,6 +15,7 @@
         {
             Console.WriteLine("Bunun IsAmg-si yoxdur.");
         }
+        Console.WriteLine($"{this.Model},{this.TopSpeed},{this.Engine},{this.Kilometers},{this.HoursePower}");
     }
-    }
+}
 
